aff52ff72127c5aa473c19f36b70afc3cba4cb17
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-04-02 09:26:10 -0300

    first commit

---
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/AutomationForPurchasingTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.automation_for_purchasing_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationForPurchasingTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationForPurchasingTicketsApplication.class, args);
+	}
+
+}
---
+++ automation_for_purchasing_tickets/src/test/java/com/example/automation_for_purchasing_tickets/AutomationForPurchasingTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automation_for_purchasing_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationForPurchasingTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
