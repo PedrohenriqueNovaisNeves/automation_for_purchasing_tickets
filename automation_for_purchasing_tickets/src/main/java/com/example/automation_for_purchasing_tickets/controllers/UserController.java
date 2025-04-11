0cb9f3bdbf1d6728eca8abb46e3c4b5d28bd8d47
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-04-11 14:34:57 -0300

    security class finalized and user listing endpoint implemented

--- automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/controllers/UserController.java
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/controllers/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -26,5 +28,11 @@ public class UserController {
         userService.saveUser(userRecord);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered with successfully");
+    }
+
+    @GetMapping("/listAllUsers")
+    public ResponseEntity<List<UserModel>> listUsers(){
+        List<UserModel> listUsers= userService.listAllUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(listUsers);
     }
 }
--- automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/security/SecurityConfig.java
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,7 +23,6 @@ public class SecurityConfig{
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
@@ -33,11 +34,16 @@ public class SecurityConfig{
     public UserDetailsService userDetailsService(){
 
         UserDetails user = User.withUsername("admin")
-                .password("admin123")
+                .password(passwordEncoder().encode("admin123"))
                 .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(user);
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
