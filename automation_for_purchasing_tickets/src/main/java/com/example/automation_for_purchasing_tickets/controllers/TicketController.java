6a11b9fced684f0e180330329a3270cc63151f65
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-04-16 08:49:13 -0300

    created endpoint for save ticket

--- automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/controllers/TicketController.java
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/controllers/TicketController.java
@@ -1,10 +1,13 @@
 package com.example.automation_for_purchasing_tickets.controllers;
 
+import com.example.automation_for_purchasing_tickets.dtos.TicketRecord;
+import com.example.automation_for_purchasing_tickets.models.TicketsModel;
 import com.example.automation_for_purchasing_tickets.services.TicketService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ticket")
@@ -18,5 +21,10 @@ public class TicketController {
         return "ping!!";
     }
 
+    @PostMapping("/saveTicket")
+    public ResponseEntity<Object> saveTicket(@Valid @RequestBody TicketRecord ticketRecord){
+        ticketService.saveTicket(ticketRecord);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Ticket registered with successfully");
+    }
 
 }
--- automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/dtos/TicketRecord.java
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/dtos/TicketRecord.java
@@ -1,6 +1,9 @@
 package com.example.automation_for_purchasing_tickets.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record TicketRecord(@NotBlank String nameTicket, @NotBlank String sector, @NotBlank String game, @NotBlank String dateOfGame) {
+import java.time.LocalDate;
+
+public record TicketRecord(@NotBlank String nameTicket, @NotBlank String sector, @NotBlank String game, @NotNull LocalDate dateOfGame) {
 }
