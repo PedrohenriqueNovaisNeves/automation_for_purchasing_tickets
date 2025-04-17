ec58fa42c765bd1c8d5fb1eae49f87a18a9f6c08
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-04-17 18:47:49 -0300

    endpoint heath check and save purchase added

---
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/controllers/BuyController.java
@@ -0,0 +1,31 @@
+package com.example.automation_for_purchasing_tickets.controllers;
+
+
+import com.example.automation_for_purchasing_tickets.dtos.BuyRecord;
+import com.example.automation_for_purchasing_tickets.models.BuyModel;
+import com.example.automation_for_purchasing_tickets.services.BuyService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/buy")
+public class BuyController {
+
+    @Autowired
+    BuyService buyService;
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong";
+    }
+
+
+    @PostMapping("/savePurchase")
+    public ResponseEntity<Object> savePurchase(@Valid @RequestBody BuyRecord buyRecord){
+        buyService.savePurchase(buyRecord);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Purchase registered with successfully");
+    }
+}
--- automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/models/BuyModel.java
+++ automation_for_purchasing_tickets/src/main/java/com/example/automation_for_purchasing_tickets/models/BuyModel.java
@@ -26,4 +26,44 @@ public class BuyModel {
     private LocalDate dateOfPurchase;
     private String purchasingMethod;
     private BigDecimal purchaseValue;
+
+    public UUID getIdBuy() {
+        return idBuy;
+    }
+
+    public String getPurchaseCode() {
+        return purchaseCode;
+    }
+
+    public LocalDate getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public String getPurchasingMethod() {
+        return purchasingMethod;
+    }
+
+    public BigDecimal getPurchaseValue() {
+        return purchaseValue;
+    }
+
+    public void setIdBuy(UUID idBuy) {
+        this.idBuy = idBuy;
+    }
+
+    public void setPurchaseCode(String purchaseCode) {
+        this.purchaseCode = purchaseCode;
+    }
+
+    public void setDateOfPurchase(LocalDate dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public void setPurchasingMethod(String purchasingMethod) {
+        this.purchasingMethod = purchasingMethod;
+    }
+
+    public void setPurchaseValue(BigDecimal purchaseValue) {
+        this.purchaseValue = purchaseValue;
+    }
 }
